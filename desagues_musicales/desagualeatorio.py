b668b4248dc20adc1c6a3f66c20097307862f357
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2016-03-26 18:05:29 +0100

    Mejoradas la bienvenida y despedida

--- desagues_musicales/desagualeatorio.py
+++ desagues_musicales/desagualeatorio.py
@@ -95,36 +95,27 @@ def cargar_musica(fichero):
 def reproducir_musica():
     canal_musica.play(musica)
 
-def parar_musica():
-    global volumen_musica
-    if reproduciendo(canal_musica):
-        volumen = volumen_musica # Se guarda el volumen original
-        volumen_musica = 0
-        for i in range(20):
-            set_panning_musica(filtrado = True)
-            time.sleep(0.05)
-        canal_musica.stop()
-        volumen_musica = volumen
-        set_panning_musica()
-
 poner_cancion = False
 def sht_detected(io):
-    global canal_izquierdo, canal_derecho, canal_musica, panning_musica, poner_cancion
+    global canal_izquierdo, canal_derecho, canal_musica, panning_musica, poner_cancion, hay_gente
+    if not hay_gente: return
     #print(io)
     poner_cancion = True
     if io == PIN_MIC_DERECHA:
-        panning_musica = 0.1
-        if not reproduciendo(canal_derecho):
-            sound = random.choice(sonidos)
-            canal_derecho.play(sound)
-            canal_derecho.set_volume(0, volumen_efectos)
+        if not reproduciendo(canal_musica) or panning_musica >= 0.8:
+            panning_musica = 0.1
+            if not reproduciendo(canal_derecho):
+                    sound = random.choice(sonidos)
+                    canal_derecho.play(sound)
+                    canal_derecho.set_volume(0, volumen_efectos)
         actualiza_contador(0,1)
     elif io == PIN_MIC_IZQUIERDA:
-        panning_musica = 0.9
-        if not reproduciendo(canal_izquierdo):
-            sound = random.choice(sonidos)
-            canal_izquierdo.play(sound)
-            canal_izquierdo.set_volume(volumen_efectos, 0)
+        if not reproduciendo(canal_musica) or panning_musica <= 0.2:
+            panning_musica = 0.9
+            if not reproduciendo(canal_izquierdo):
+                sound = random.choice(sonidos)
+                canal_izquierdo.play(sound)
+                canal_izquierdo.set_volume(volumen_efectos, 0)
         actualiza_contador(1,0)
 
 
@@ -152,7 +143,7 @@ def set_panning_musica(filtrado = False):
     global panning_musica_filtrado, canal_musica, volumen_musica_filtrado
     if filtrado:
         panning_musica_filtrado = panning_musica_filtrado * 0.8 + panning_musica * 0.2
-        volumen_musica_filtrado = volumen_musica_filtrado * 0.8 + volumen_musica * 0.2
+        volumen_musica_filtrado = volumen_musica_filtrado * 0.6 + volumen_musica * 0.4
     else:
         panning_musica_filtrado = panning_musica
         volumen_musica_filtrado = volumen_musica
@@ -161,53 +152,107 @@ def set_panning_musica(filtrado = False):
     canal_musica.set_volume(L,R)
 
 
+def hay_luz(): # TO-DO: incorporar sensor de luz
+    if DEBUG: return False
+    
+    return WTF
+
 
 random.seed()
 
-if not DEBUG:
-    display_izquierdo.set_text_centered("hola")
-    display_derecho.set_text_centered("hola")
-
-cargar_musica("./sonidos/portal_turret_salute.ogg")
-reproducir_musica()
-
-canal_musica.set_volume(volumen_efectos, 0)
-if not DEBUG:
-    display_izquierdo.parpadea(LED_COLOR_ORANGE)
-
-time.sleep(1.1)
-
-canal_musica.set_volume(0, volumen_efectos)
-if not DEBUG:
-    display_derecho.parpadea(LED_COLOR_ORANGE)
-
-
-MUSICA_DE_BIENVENIDA = True
-if MUSICA_DE_BIENVENIDA:
-    fichero = random.choice(musica_de_bienvenida)
-    cargar_musica(fichero)
-    while reproduciendo(canal_musica):
-        time.sleep(0.1)
-    reproducir_musica()
-    canal_musica.set_volume(volumen_musica, volumen_musica)
-
-time.sleep(5)
-
-if not DEBUG:
-    display_izquierdo.set_text_centered("start")
-    display_derecho.set_text_centered("start")
-    display_izquierdo.color_leds(LED_COLOR_GREEN)
-    display_derecho.color_leds(LED_COLOR_GREEN)
+hay_gente = False
 
 if not DEBUG:
     GPIO.add_event_detect(PIN_MIC_DERECHA, GPIO.RISING, callback=sht_detected, bouncetime=500)
     GPIO.add_event_detect(PIN_MIC_IZQUIERDA, GPIO.RISING, callback=sht_detected, bouncetime=500)
 
-fichero = random.choice(lista_musica)
-cargar_musica(fichero)
 
-#sht_detected(PIN_MIC_IZQUIERDA)
-#sht_detected(PIN_MIC_DERECHA)
+def bienvenida():
+    global hay_gente, contador_derecho, contador_izquierdo, panning_musica
+    if not DEBUG:
+        display_derecho.enable(6)
+        display_izquierdo.enable(6)
+        display_izquierdo.set_text_centered("hola")
+        display_derecho.set_text_centered("hello")
+
+    cargar_musica("./sonidos/portal_turret_salute.ogg")
+    reproducir_musica()
+
+    canal_musica.set_volume(volumen_efectos, 0)
+    if not DEBUG:
+        display_izquierdo.parpadea(LED_COLOR_ORANGE)
+
+    time.sleep(1.1)
+
+    canal_musica.set_volume(0, volumen_efectos)
+    if not DEBUG:
+        display_derecho.parpadea(LED_COLOR_ORANGE)
+
+
+    MUSICA_DE_BIENVENIDA = True
+    if MUSICA_DE_BIENVENIDA:
+        fichero = random.choice(musica_de_bienvenida)
+        cargar_musica(fichero)
+        while reproduciendo(canal_musica):
+            time.sleep(0.1)
+        reproducir_musica()
+        panning_musica = 0.5
+        set_panning_musica()
+
+    if DEBUG:
+        time.sleep(6)
+    else:
+        colors = [LED_COLOR_RED, LED_COLOR_GREEN, LED_COLOR_ORANGE]
+        for i in range(3):
+            display_izquierdo.parpadea(colors[i], n=4, delay=0.25)
+            display_derecho.parpadea(colors[i], n=4, delay=0.25)
+        display_izquierdo.set_text_centered("start")
+        display_derecho.set_text_centered("start")
+        display_izquierdo.color_leds(LED_COLOR_GREEN)
+        display_derecho.color_leds(LED_COLOR_GREEN)
+
+    contador_izquierdo = 0
+    contador_derecho = 0
+
+    fichero = random.choice(lista_musica)
+    cargar_musica(fichero)
+
+    hay_gente = True
+
+
+def adios():
+    global hay_gente
+
+    cargar_musica("./sonidos/portal_turret.ogg")
+    reproducir_musica()
+    canal_musica.set_volume(volumen_efectos, volumen_efectos)
+    time.sleep(3)
+
+    if hay_luz(): return
+    hay_gente = False
+
+    if not DEBUG:
+        display_izquierdo.set_text_centered("hasta")
+        display_derecho.set_text_centered("pronto")
+
+    while reproduciendo(canal_musica):
+        time.sleep(0.1)
+    cargar_musica("./sonidos/worms_bye_bye.ogg")
+    reproducir_musica()
+    canal_musica.set_volume(volumen_efectos, 0)
+
+    time.sleep(5)
+
+    if not DEBUG: # apagar los displays
+        display_izquierdo.set_text_centered("")
+        display_derecho.set_text_centered("")
+        display_izquierdo.color_leds(0)
+        display_derecho.color_leds(0)
+
+
+bienvenida()
+
+volumen_backup = volumen_musica
 
 while True:
     if DEBUG:
@@ -218,17 +263,32 @@ while True:
                 elif event.key == pygame.K_RIGHT:
                     sht_detected(PIN_MIC_DERECHA)
                 elif event.key == pygame.K_DOWN:
-                    parar_musica()
+                    volumen_musica = 0
+                elif event.key == pygame.K_UP:
+                    if hay_gente:
+                        adios()
+                    else:
+                        bienvenida()
+    else:
+        if hay_luz() and not hay_gente:
+            bienvenida()
+        if not hay_luz() and hay_gente:
+            adios()
+    
     set_panning_musica(filtrado = True)
     time.sleep(0.3)
     if reproduciendo(canal_musica):
         poner_cancion = False
+        if volumen_musica_filtrado < 0.1:
+            canal_musica.stop()
+    else:
+        volumen_musica = volumen_backup
     if poner_cancion:
-        time.sleep(2)
-        reproducir_musica()
-        set_panning_musica()
-        fichero = random.choice(lista_musica)
-        cargar_musica(fichero)
+        if not (reproduciendo(canal_izquierdo) or reproduciendo(canal_derecho)):
+            reproducir_musica()
+            set_panning_musica()
+            fichero = random.choice(lista_musica)
+            cargar_musica(fichero)
 
 if not DEBUG:
     GPIO.cleanup()
