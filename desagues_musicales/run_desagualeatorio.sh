f8e2d49b326508b705da2d7e9ed079cc88cd9c00
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2018-02-27 17:55:34 +0100

    Update run_desagualeatorio.sh

--- desagues_musicales/run_desagualeatorio.sh
+++ desagues_musicales/run_desagualeatorio.sh
@@ -5,15 +5,10 @@ RESULT=`ps -a | sed -n /${SERVICE}/p`
 
 if [ "${RESULT:-null}" = null ]; then
     echo "Launching script $0"
-    while true
-    do
-      # Volumen de reproduccion al valor deseado (maximo: 100%)
-      amixer set PCM 98%
-      cd "$HOME/Codigos/automatismos-del-local/desagues_musicales"
-      python desagualeatorio.py
-      sleep 1
-    done
+    # Volumen de reproduccion al valor deseado (maximo: 100%)
+    amixer set PCM 98%
+    cd "$HOME/Codigos/automatismos-del-local/desagues_musicales"
+    python desagualeatorio.py
 else
     echo "Script $0 already running"
 fi
-
